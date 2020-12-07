a39a7da1f4ea9d3c55994d1238dee1146b5189c8
reybaliguat <rey@ajio.co.jp>
2020-12-07 15:21:51 +0800

    Updates

---
+++ precache-manifest.d84877866bb6332091bcc7c09e6f81f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22acddf6ae237927d05cf04ed500442a",
+    "url": "/RecipeReactApp/index.html"
+  },
+  {
+    "revision": "4a5e3f4b08740fa2dbd8",
+    "url": "/RecipeReactApp/static/js/2.cbb14223.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RecipeReactApp/static/js/2.cbb14223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f38cba5aef39d02cb3",
+    "url": "/RecipeReactApp/static/js/main.1f258d20.chunk.js"
+  },
+  {
+    "revision": "4ce61aefe4eb42c69be1",
+    "url": "/RecipeReactApp/static/js/runtime-main.11622d2a.js"
+  }
+]);
